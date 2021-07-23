185e21bf61d0a1a53cb1ec12074fe7215e8c08cd
Taiane Rodrigues <taianeapdarodrigues@gmail.com>
2021-07-23 01:14:54 -0300

    Add files via upload

---
+++ AtividadeFinalTAP/AtividadeFinalTAP/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace AtividadeFinalTAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.inputPesquisa = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPesquisar.Location = new System.Drawing.Point(210, 35);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(87, 30);
+            this.btnPesquisar.TabIndex = 0;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // inputPesquisa
+            // 
+            this.inputPesquisa.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputPesquisa.Location = new System.Drawing.Point(27, 35);
+            this.inputPesquisa.Multiline = true;
+            this.inputPesquisa.Name = "inputPesquisa";
+            this.inputPesquisa.Size = new System.Drawing.Size(177, 30);
+            this.inputPesquisa.TabIndex = 1;
+            this.inputPesquisa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inputPesquisa.TextChanged += new System.EventHandler(this.inputPesquisa_TextChanged);
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text.Location = new System.Drawing.Point(51, 101);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(0, 21);
+            this.text.TabIndex = 2;
+            this.text.Click += new System.EventHandler(this.text_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Resultado:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(328, 232);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.inputPesquisa);
+            this.Controls.Add(this.btnPesquisar);
+            this.Name = "Form1";
+            this.Text = "Consulta CEP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox inputPesquisa;
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AtividadeFinalTAP/AtividadeFinalTAP/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeFinalTAP
+{
+    using servCEP;
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            using (var ws = new AtendeClienteClient())
+            {
+                var resposta = ws.consultaCEP(inputPesquisa.Text);
+                text.Text = "Endereco: " + resposta.end + "\nBairro: " + resposta.bairro + "\nCidade: " + resposta.cidade;
+            }
+
+            
+        }
+
+        private void inputPesquisa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
